5de4eb777a17661c98b318149fe2490ddd8ca27b
Patrick Niebrzydowski <patrickniebrzydowski@Pulpmedias-Mac-mini.local>
2018-06-18 16:22:39 +0200

    add firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+
+const config = {
+    apiKey: "AIzaSyCU0dpMDsC__3z2ocqAr9jSAET0jLCl7Z4",
+    authDomain: "infinite-tortoise-and-hare.firebaseapp.com",
+    databaseURL: "https://infinite-tortoise-and-hare.firebaseio.com",
+    projectId: "infinite-tortoise-and-hare",
+    storageBucket: "infinite-tortoise-and-hare.appspot.com",
+    messagingSenderId: "65125901892"
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+}
+
+const firestore = firebase.firestore();
+
+export {
+    firestore
+};
